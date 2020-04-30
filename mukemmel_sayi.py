ce89137b94c96197fbcbefd4d8e6706c06083b86
Çağatay Ceyhan <58742177+cagatayceyhan@users.noreply.github.com>
2020-04-30 11:18:42 -0400

    Mükemmel Sayı Bulma

---
+++ mukemmel_sayi.py
@@ -0,0 +1,12 @@
+sayı = int(input("Sayı Giriniz: "))
+
+i = 1
+toplam = 0
+while (i<sayı):
+    if(sayı %i == 0):
+        toplam += i
+    i += 1
+if(toplam == sayı):
+    print(sayı,"Mükemmel Bir Sayıdır.")
+else:
+    print(sayı,"Mükemmel Sayı Değildir.")
